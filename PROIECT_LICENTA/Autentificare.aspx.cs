1c8a4123f44aa49cd4bd05a34ba98db4e4f7ff95
iuliahasan <iuliahasan56@gmail.com>
2025-01-22 11:29:41 +0200

    Add project files.

---
+++ PROIECT_LICENTA/Autentificare.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace PROIECT_LICENTA
+{
+    public partial class Autentificare : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(TextBoxEmail.Text, @"\A(?:[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z"))
+            {
+                if(Regex.IsMatch(TextBoxPassword.Text, @"^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@.#$!%?&])[A-Za-z\d@.#$!%?&]{8,15}$"))
+                {
+                        
+                }
+                else
+                {
+                    LabelMessage.Text = "Incorect password";
+                }
+            }
+            else
+            {
+                LabelMessage.Text = "Incorrect email";
+            }
+        }
+    }
+}
---
+++ PROIECT_LICENTA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROIECT_LICENTA.Global" Language="C#" %>
